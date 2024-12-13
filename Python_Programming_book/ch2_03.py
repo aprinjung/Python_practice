b8f05e7e5439cbfa4eefb88f90d662d667943286
Jiyoon Jung <jioyun1021@gmail.com>
2024-12-13 23:09:39 +0900

    commit 2
    변수와 자료형

---
+++ Python_Programming_book/ch2_03.py
@@ -0,0 +1,5 @@
+celsius = int(input("섭씨온도를 입력하세요 : "))
+fahrenheit = ((9/5)*celsius)+32
+print("섭씨온도:",celsius, "화씨온도:", fahrenheit)
+
+23days = 50
---
+++ Python_Programming_book/ch2_14.py
@@ -0,0 +1,5 @@
+a = 10
+b = 30
+c = 10.5
+print(a*b, str(c))
+print(str(a*b)+str(c))
