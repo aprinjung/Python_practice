d0836356af9c5bbc01514394d45958a206f5f72d
Jiyoon Jung <jioyun1021@gmail.com>
2024-12-25 21:27:55 +0900

    commit 7

--- Python_Programming_book/ch03_06.py
+++ Python_Programming_book/ch03_06.py
@@ -1,4 +1,6 @@
 user_input_a = input("첫 번째 값을 입력하시오 : ")
 user_input_b = input("두 번째 값을 입력하시오 : ")
 total = user_input_a + user_input_b
-print(total)
+total
+# 코드 편집기에서는 print를 꼭 넣어야 하는데 
+# jupyter에선 그냥 출력됨.
