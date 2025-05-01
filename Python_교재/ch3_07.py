1f599930fe141e3c89c18ece6f054b10112bc7f6
Jiyoon Jung <jioyun1021@gmail.com>
2025-05-01 21:05:26 +0900

    250501

---
+++ Python_교재/ch3_07.py
@@ -0,0 +1,8 @@
+alphabet = ['a', 'b', 'c', 'd']
+print(alphabet*2)
+# print(alphabet.remove(alphabet)) #ValueError: list.remove(x): x not in list
+print(alphabet+alphabet)
+# print(alphabet.append(alphabet))
+print(alphabet*len(alphabet[1:3]))
+
+# ['a','b','c','d','a','b','c','d']
